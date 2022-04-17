4493d8a4232257ff35305b1c9ee3d137b7ca82b0
Rahul Rawat <rahulsinghrawat760@gmail.com>
2022-04-18 00:28:19 +0530

    feat : added homepage

---
+++ src/pages/Home/Home.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { Sidebar } from "../../components";
+import { VideoCard } from "../../components";
+import "./Home.css";
+
+export const Home = () => {
+	return (
+		<div className="home-container">
+			<Sidebar />
+			<div className="video-listing-container">
+				<VideoCard />
+				<VideoCard />
+				<VideoCard />
+				<VideoCard />
+				<VideoCard />
+				<VideoCard />
+				<VideoCard />
+				<VideoCard />
+				<VideoCard />
+				<VideoCard />
+				<VideoCard />
+				<VideoCard />
+			</div>
+		</div>
+	);
+};
