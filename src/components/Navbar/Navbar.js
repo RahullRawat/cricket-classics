3d57ea1a80e158072f1010493c5fea4a97edfc55
Rahul Rawat <rahulsinghrawat760@gmail.com>
2022-04-18 00:27:33 +0530

    feat : added nav component

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "./Navbar.css";
+
+const Navbar = () => {
+	return (
+		<div className="navbar-container">
+			<div className="nav-brand-title">
+				<a href="#">
+					Cricket <span>Classics</span>
+				</a>
+			</div>
+			<div className="search-bar-container">
+				<i class="fa-solid fa-magnifying-glass"></i>
+				<input type="text" />
+			</div>
+			<div className="profile-container">
+				<i class="fa-solid fa-user"></i>
+			</div>
+		</div>
+	);
+};
+
+export { Navbar };
