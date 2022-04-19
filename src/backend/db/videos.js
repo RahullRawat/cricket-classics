1cf5ae3e6059b0848203bc8297eb71fa468fdf4f
Rahul Rawat <rahulsinghrawat760@gmail.com>
2022-04-19 16:51:53 +0530

    feat : added videos to database

--- src/backend/db/videos.js
+++ src/backend/db/videos.js
@@ -4,18 +4,294 @@
  * */
 
 export const videos = [
-  {
-    _id: "Wo5dMEP_BbI",
-    title: "Awesome Video about Coding",
-    description:
-      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
-    creator: "Soham Shah",
-  },
-  {
-    _id: "F_Riqjdh2oM",
-    title: "Neural Networks from Scratch - P.1 Intro and Neuron Code",
-    creator: "Sentdex",
-    description:
-      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
-  },
+	{
+		_id: "Wo01dMEP_BbI",
+		title: "Stars crush Hurricanes as Maxwell makes history | BBL|11",
+		channelName: "cricket.com.au",
+		thumbnail:
+			"https://i.ytimg.com/an_webp/TlZwCVCuSEE/mqdefault_6s.webp?du=3000&sqp=CLbZ-ZIG&rs=AOn4CLDSQYAzsbwARzzxxd0Wk0N_-PjZYQ",
+		channelAvatar:
+			"https://yt3.ggpht.com/ytc/AKedOLSaliAXEasGek2J2eYdjkofH1rvdqZ4V320tT-NVRU=s88-c-k-c0x00ffffff-no-rj",
+		description:
+			"At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
+		videoViews: "4.9M views",
+		likes: "114k",
+		time: "11.49",
+	},
+
+	{
+		_id: "Wo02dMEP_BbI",
+		title:
+			"Semi Final 1 | Trinbago Knight Riders vs Saint Lucia Kings | CPL 2021",
+		channelName: "CPL T20",
+		thumbnail:
+			"https://i.ytimg.com/an_webp/FEk6TEJSll8/mqdefault_6s.webp?du=3000&sqp=CMTU-ZIG&rs=AOn4CLD3aly14jIM2tS5u9BFUZXNvJsx9g",
+		channelAvatar:
+			"https://yt3.ggpht.com/yRMhS5M9ZV9nFW4vr5sNcnGxCgNgKEJ20dFKGYwL6vO73z0O26HwJ7VJV_B6rGW9ulGBzY_YJg=s176-c-k-c0x00ffffff-no-rj-mo",
+		description:
+			"At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
+		videoViews: "918k views",
+		likes: "18k",
+		time: "8.03",
+	},
+	{
+		_id: "Wo03dMEP_BbI",
+		title: "10 Sixes for Livingstone! | The Hundred 21",
+		channelName: "The Hundred",
+		thumbnail:
+			"https://i.ytimg.com/an_webp/KA4n5wej5Qc/mqdefault_6s.webp?du=3000&sqp=CPeJ-pIG&rs=AOn4CLAf5PBP8wQUOFBsIg7j1q4BozznzA",
+		channelAvatar:
+			"https://yt3.ggpht.com/ytc/AKedOLRIZViEjjCoFajSaJY_7A4MDNloQ8ngQ7FgmLlF=s48-c-k-c0x00ffffff-no-rj",
+		description:
+			"At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
+		videoViews: "820k views",
+		likes: "16k",
+		time: "14.45",
+	},
+	{
+		_id: "Wo04dMEP_BbI",
+		title:
+			"Rohit Sharma Hits 140! | India v Pakistan | ICC Cricket World Cup 2019",
+		channelName: "ICC",
+		thumbnail:
+			"https://i.ytimg.com/an_webp/AFEZzf9_EHk/mqdefault_6s.webp?du=3000&sqp=CLmR-pIG&rs=AOn4CLAQGCv-bnfbg7nW4yXKo0ISvf9hVw",
+		channelAvatar:
+			"https://yt3.ggpht.com/ytc/AKedOLQW1DW8YJh1nhWtC3uVnvqZ4lzjiMXUoxbp_U4mfjs=s48-c-k-c0x00ffffff-no-rj",
+		description:
+			"At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
+		videoViews: "90M views",
+		likes: "716k",
+		time: "6.14",
+	},
+	{
+		_id: "Wo05dMEP_BbI",
+		title:
+			"The FINAL | Deccan Gladiators vs Delhi Bulls | Day 15 | Abu Dhabi T10",
+		channelName: "T10 League Official",
+		thumbnail:
+			"https://i.ytimg.com/an_webp/jJUBYUeJ0l0/mqdefault_6s.webp?du=3000&sqp=CJLU-ZIG&rs=AOn4CLCMQ2GAATHSCcSECNoaAebFpa0URg",
+		channelAvatar:
+			"https://yt3.ggpht.com/qEjZkQtskTBkX4wq-Br4Z9OG9QOVJto_O3rSpYaa_8FzImGunyam7rTnMXMFZl4ymQjalU4A=s48-c-k-c0x00ffffff-no-rj",
+		description:
+			"At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
+		videoViews: "2M views",
+		likes: "40k",
+		time: "22.00",
+	},
+	{
+		_id: "Wo06dMEP_BbI",
+		title: "Perth Scorchers vs Sydney Sixers | Highlights | BBL | Final |",
+		channelName: "cricket.com.au",
+		thumbnail:
+			"https://i.ytimg.com/an_webp/iV1AdFS-Zak/mqdefault_6s.webp?du=3000&sqp=CPSj-ZIG&rs=AOn4CLA9fjqyKfxTC5iecY28rtFI627wTw",
+		channelAvatar:
+			"https://yt3.ggpht.com/ytc/AKedOLSaliAXEasGek2J2eYdjkofH1rvdqZ4V320tT-NVRU=s88-c-k-c0x00ffffff-no-rj",
+		description:
+			"At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
+		videoViews: "1.09M views",
+		likes: "25k",
+		time: "16.05",
+	},
+	{
+		_id: "Wo07dMEP_BbI",
+		title:
+			"Semi Final 2 | Guyana Amazon Warriors vs St Kitts & Nevis Patriots | CPL 2021",
+		channelName: "CPL T20",
+		thumbnail:
+			"https://i.ytimg.com/an_webp/BNbNKygcG-A/mqdefault_6s.webp?du=3000&sqp=CMDj-ZIG&rs=AOn4CLDtSNcM-57AIesvt6PW37NpUTMsyg",
+		channelAvatar:
+			"https://yt3.ggpht.com/yRMhS5M9ZV9nFW4vr5sNcnGxCgNgKEJ20dFKGYwL6vO73z0O26HwJ7VJV_B6rGW9ulGBzY_YJg=s48-c-k-c0x00ffffff-no-rj",
+		description:
+			"At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
+		videoViews: "1.3 views",
+		likes: "40k",
+		time: "8.03",
+	},
+	{
+		_id: "Wo08dMEP_BbI",
+		title: "Southern Brave Are Champions! | The Hundred 2021",
+		channelName: "The Hundred",
+		thumbnail:
+			"https://i.ytimg.com/an_webp/SomR0nVmi9w/mqdefault_6s.webp?du=3000&sqp=COTs-ZIG&rs=AOn4CLBw7G3GbILsoGCj_493fMfx-9Jafg",
+		channelAvatar:
+			"https://yt3.ggpht.com/ytc/AKedOLRIZViEjjCoFajSaJY_7A4MDNloQ8ngQ7FgmLlF=s48-c-k-c0x00ffffff-no-rj",
+		description:
+			"At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
+		videoViews: "824k views",
+		likes: "17k",
+		time: "14.51",
+	},
+	{
+		_id: "Wo09dMEP_BbI",
+		title: "Yuvraj Singh slams six sixes off Stuart Broad | T20 World Cup 2007",
+		channelName: "ICC",
+		thumbnail:
+			"https://i.ytimg.com/an_webp/8b0ubLO2MUE/mqdefault_6s.webp?du=3000&sqp=COeM-pIG&rs=AOn4CLDqnVNHVvJ4W6ATQaQ9yrNPIf7y2w",
+		channelAvatar:
+			"https://yt3.ggpht.com/ytc/AKedOLQW1DW8YJh1nhWtC3uVnvqZ4lzjiMXUoxbp_U4mfjs=s48-c-k-c0x00ffffff-no-rj",
+		description:
+			"At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
+		videoViews: "63M views",
+		likes: "1.1M",
+		time: "6.44",
+	},
+	{
+		_id: "Wo10dMEP_BbI",
+		title:
+			"Deccan Gladiators vs Team Abu Dhabi | Day 11 | Abu Dhabi T10 Season 5",
+		channelName: "T10 League Official",
+		thumbnail:
+			"https://i.ytimg.com/an_webp/EZbIJxVMHDk/mqdefault_6s.webp?du=3000&sqp=COn--ZIG&rs=AOn4CLBcuvg1vfZVnEn5hjTDS-FhtuTHMQ",
+		channelAvatar:
+			"https://yt3.ggpht.com/qEjZkQtskTBkX4wq-Br4Z9OG9QOVJto_O3rSpYaa_8FzImGunyam7rTnMXMFZl4ymQjalU4A=s48-c-k-c0x00ffffff-no-rj",
+		description:
+			"At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
+		videoViews: "1.9M views",
+		likes: "28k",
+		time: "22.00",
+	},
+	{
+		_id: "Wo11dMEP_BbI",
+		title: "Sixers thrash Stars in record-breaking display | BBL|11",
+		channelName: "cricket.com.au",
+		thumbnail:
+			"https://i.ytimg.com/an_webp/JRBhVA6oVdw/mqdefault_6s.webp?du=3000&sqp=CLTl-ZIG&rs=AOn4CLDTnO1SWlkZqCIRU1OPtn2yG8EkRw",
+		channelAvatar:
+			"https://yt3.ggpht.com/ytc/AKedOLSaliAXEasGek2J2eYdjkofH1rvdqZ4V320tT-NVRU=s88-c-k-c0x00ffffff-no-rj",
+		description:
+			"At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
+		videoViews: "1.6 views",
+		likes: "40k",
+		time: "8.29",
+	},
+	{
+		_id: "Wo12dMEP_BbI",
+		title: "Final | Saint Lucia Kings vs St. Kitts & Nevis Patriots | CPL 2021",
+		channelName: "CPL T20",
+		thumbnail:
+			"https://i.ytimg.com/an_webp/6oQo8lf5USM/mqdefault_6s.webp?du=3000&sqp=CImG-pIG&rs=AOn4CLAq6c6QD9qCWvHMoL_W6MdMqfMpZQ",
+		channelAvatar:
+			"https://yt3.ggpht.com/yRMhS5M9ZV9nFW4vr5sNcnGxCgNgKEJ20dFKGYwL6vO73z0O26HwJ7VJV_B6rGW9ulGBzY_YJg=s48-c-k-c0x00ffffff-no-rj",
+		description:
+			"At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
+		videoViews: "1.3M views",
+		likes: "32k",
+		time: "8.03",
+	},
+	{
+		_id: "Wo13dMEP_BbI",
+		title: "Oval Invincibles vs Manchester Originals | The Hundred 2021",
+		channelName: "The Hundred",
+		thumbnail:
+			"https://i.ytimg.com/an_webp/3RtbHPSr8HU/mqdefault_6s.webp?du=3000&sqp=CLuH-pIG&rs=AOn4CLD5mtHmtn-fJU6u8AQi-lWIoLGeGg",
+		channelAvatar:
+			"https://yt3.ggpht.com/ytc/AKedOLRIZViEjjCoFajSaJY_7A4MDNloQ8ngQ7FgmLlF=s48-c-k-c0x00ffffff-no-rj",
+		description:
+			"At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
+		videoViews: "1.2M views",
+		likes: "28k",
+		time: "15.26",
+	},
+	{
+		_id: "Wo14dMEP_BbI",
+		title: "Biggest Sixes! | ICC Cricket World Cup 2019",
+		channelName: "ICC",
+		thumbnail:
+			"https://i.ytimg.com/an_webp/fBIqzpkaIy8/mqdefault_6s.webp?du=3000&sqp=CMif-pIG&rs=AOn4CLCwaYPM-pgcXR3nmrDfzq0WB8JuRA",
+		channelAvatar:
+			"https://yt3.ggpht.com/ytc/AKedOLQW1DW8YJh1nhWtC3uVnvqZ4lzjiMXUoxbp_U4mfjs=s48-c-k-c0x00ffffff-no-rj",
+		description:
+			"At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
+		videoViews: "7M views",
+		likes: "125k",
+		time: "2.16",
+	},
+	{
+		_id: "Wo15dMEP_BbI",
+		title: "Bangla Tigers vs Team Abu Dhabi | Day 1 | Abu Dhabi T10 | Season 5",
+		channelName: "T10 League Official",
+		thumbnail:
+			"https://i.ytimg.com/an_webp/zXddZxAkXz4/mqdefault_6s.webp?du=3000&sqp=CPrx-ZIG&rs=AOn4CLBkWHiPyQIpXpifMbaYVQP9hZLrBQ",
+		channelAvatar:
+			"https://yt3.ggpht.com/qEjZkQtskTBkX4wq-Br4Z9OG9QOVJto_O3rSpYaa_8FzImGunyam7rTnMXMFZl4ymQjalU4A=s48-c-k-c0x00ffffff-no-rj",
+		description:
+			"At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
+		videoViews: "1.7M views",
+		likes: "34k",
+		time: "22.00",
+	},
+	{
+		_id: "Wo16dMEP_BbI",
+		title:
+			"Hurricanes knock off defending champs in BBL|10 opener | KFC BBL|10",
+		channelName: "cricket.com.au",
+		thumbnail:
+			"https://i.ytimg.com/an_webp/6xiQcBhEtnw/mqdefault_6s.webp?du=3000&sqp=CMjZ-ZIG&rs=AOn4CLANV5ywzWiULafBfVbsCsh01fQlcA",
+		channelAvatar:
+			"https://yt3.ggpht.com/ytc/AKedOLSaliAXEasGek2J2eYdjkofH1rvdqZ4V320tT-NVRU=s48-c-k-c0x00ffffff-no-rj",
+		description:
+			"At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
+		videoViews: "4M views",
+		likes: "77k",
+		time: "7.54",
+	},
+	{
+		_id: "Wo17dMEP_BbI",
+		title: "Odean Smith Breaking Bats & Smashing Sixes!",
+		channelName: "CPL T20",
+		thumbnail:
+			"https://i.ytimg.com/an_webp/Qv6eRZ2hQ9E/mqdefault_6s.webp?du=3000&sqp=CNHz-ZIG&rs=AOn4CLCAYYgLwyk6MV4H2WZT9rYgpuummA",
+		channelAvatar:
+			"https://yt3.ggpht.com/yRMhS5M9ZV9nFW4vr5sNcnGxCgNgKEJ20dFKGYwL6vO73z0O26HwJ7VJV_B6rGW9ulGBzY_YJg=s48-c-k-c0x00ffffff-no-rj",
+		description:
+			"At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
+		videoViews: "4.1M views",
+		likes: "34k",
+		time: "5.10",
+	},
+	{
+		_id: "Wo18dMEP_BbI",
+		title:
+			"Biggest Total Yet! | London Spirit v Northern Superchargers | The Hundred",
+		channelName: "The Hundred",
+		thumbnail:
+			"https://i.ytimg.com/an_webp/gQCNYyDbew0/mqdefault_6s.webp?du=3000&sqp=CKaW-pIG&rs=AOn4CLDnhZCY6kwV1nf-2BX3-22wrLmzjQ",
+		channelAvatar:
+			"https://yt3.ggpht.com/ytc/AKedOLRIZViEjjCoFajSaJY_7A4MDNloQ8ngQ7FgmLlF=s48-c-k-c0x00ffffff-no-rj",
+		description:
+			"At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
+		videoViews: "313k views",
+		likes: "5.9k",
+		time: "14.52",
+	},
+	{
+		_id: "Wo19dMEP_BbI",
+		title: "England Win CWC After Super Over! | ICC Cricket World Cup 2019",
+		channelName: "ICC",
+		thumbnail:
+			"https://i.ytimg.com/an_webp/Kwu1yIC-ssg/mqdefault_6s.webp?du=3000&sqp=CIiY-pIG&rs=AOn4CLCYoAti6w1gQGElxx9iKVa7ft7xwQ",
+		channelAvatar:
+			"https://yt3.ggpht.com/ytc/AKedOLQW1DW8YJh1nhWtC3uVnvqZ4lzjiMXUoxbp_U4mfjs=s48-c-k-c0x00ffffff-no-rj",
+		description:
+			"At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
+		videoViews: "44M views",
+		likes: "367k",
+		time: "8.09",
+	},
+
+	{
+		_id: "Wo20dMEP_BbI",
+		title:
+			"The Chennai Braves vs Team Abu Dhabi | Day 4 | Abu Dhabi T10 Season 5",
+		channelName: "T10 League Official",
+		thumbnail:
+			"https://i.ytimg.com/an_webp/HkR5u-zoxjM/mqdefault_6s.webp?du=3000&sqp=CMmU-pIG&rs=AOn4CLDZGuXN1VA2OFg8BCNMhAhMgwz5Ug",
+		channelAvatar:
+			"https://yt3.ggpht.com/qEjZkQtskTBkX4wq-Br4Z9OG9QOVJto_O3rSpYaa_8FzImGunyam7rTnMXMFZl4ymQjalU4A=s48-c-k-c0x00ffffff-no-rj",
+		description:
+			"At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
+		videoViews: "783k views",
+		likes: "14k",
+		time: "21.44",
+	},
 ];
