a649b50cebf7f15b0969accd12e53e77bdf10f1c
Rahul Rawat <rahulsinghrawat760@gmail.com>
2022-04-22 02:26:19 +0530

    feat : playlist page

---
+++ src/pages/Playlist/Playlist.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { Sidebar } from "../../components";
+import "./Playlist.css";
+
+export const Playlist = () => {
+	return (
+		<div className="playlist-wrapper">
+			<Sidebar />
+			<div className="playlist-display-container ">
+				<div className="playlist-thumbnail ">
+					<img
+						src="https://i3.ytimg.com/vi/FEk6TEJSll8/maxresdefault.jpg"
+						alt=""
+					/>
+					<div className="playlist-videos">
+						<span className="playlist-videos-count">100</span>
+						<i className="fa-solid fa-list-check playlist-icon"></i>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
