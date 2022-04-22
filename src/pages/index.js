6c003d8aee5a5a23ed169cacf3d697db0aa1de32
Rahul Rawat <rahulsinghrawat760@gmail.com>
2022-04-22 15:45:48 +0530

    feat : exported liked, history and watchlater page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,2 +1,5 @@
 export { Home } from "./Home/Home";
 export { Playlist } from "./Playlist/Playlist";
+export { LikedVideo } from "./LikedVideos/LikedVideo";
+export { History } from "./History/History";
+export { WatchLater } from "./WatchLater/WatchLater";
