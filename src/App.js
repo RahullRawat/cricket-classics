1566eb6a534c84bdf501efc6c6580a00ef5f0315
Rahul Rawat <rahulsinghrawat760@gmail.com>
2022-04-22 15:43:56 +0530

    feat : added liked, history and watchlater page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import { Navbar } from "./components/index";
-import { Home, Playlist } from "./pages/index";
+import { Home, Playlist, LikedVideo, History, WatchLater } from "./pages/index";
 import { Routes, Route } from "react-router-dom";
 
 function App() {
@@ -10,6 +10,9 @@ function App() {
 			<Routes>
 				<Route path="/" element={<Home />} />
 				<Route path="/playlist" element={<Playlist />} />
+				<Route path="/liked" element={<LikedVideo />} />
+				<Route path="/history" element={<History />} />
+				<Route path="/watchlater" element={<WatchLater />} />
 			</Routes>
 		</div>
 	);
