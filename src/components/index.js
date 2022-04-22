48249ab4408394b32e111af96036fa62ce645e33
Rahul Rawat <rahulsinghrawat760@gmail.com>
2022-04-22 15:44:42 +0530

    feat : exported horizontalVideoCard component

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
 export { Navbar } from "./Navbar/Navbar";
 export { Sidebar } from "./Sidebar/Sidebar";
 export { VideoCard } from "./VideoCard/VideoCard";
+export { HorizontalVideoCard } from "./HorizontalVideoCard/HorizontalVideoCard";
