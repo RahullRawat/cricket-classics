71a1ce87ac199d438e9dd4081371fb4a5955d7e6
Rahul Rawat <rahulsinghrawat760@gmail.com>
2022-04-25 23:57:26 +0530

    feat : user page and css

---
+++ src/pages/User/User.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { useAuth } from "../../context/AuthContext";
+import "./User.css";
+
+export const User = () => {
+	const { authState, signOut } = useAuth();
+	return (
+		<div className="user-profile-wrapper">
+			<section className="user-profile-container ">
+				<div className="profile-details">
+					<div className="username-container">
+						<h5>Name - </h5>
+						{authState.userData && (
+							<h6 className="username">
+								{authState.userData.firstName + authState.userData.lastName}
+							</h6>
+						)}
+					</div>
+					<div className="user-email-container">
+						<h5>Email - </h5>
+						{authState.userData && (
+							<h6 className="email">{authState.userData.email}</h6>
+						)}
+					</div>
+				</div>
+				<button className="btn-primary btn btn-logout" onClick={signOut}>
+					LogOut
+				</button>
+			</section>
+		</div>
+	);
+};
