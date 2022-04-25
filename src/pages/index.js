d6b92b01bc2577143a0aa143b8bc3d0350328ae5
Rahul Rawat <rahulsinghrawat760@gmail.com>
2022-04-25 23:56:42 +0530

    feat : exported auth pages and user page

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,3 +3,6 @@ export { Playlist } from "./Playlist/Playlist";
 export { LikedVideo } from "./LikedVideos/LikedVideo";
 export { History } from "./History/History";
 export { WatchLater } from "./WatchLater/WatchLater";
+export { Login } from "./Authentication/Login";
+export { Signup } from "./Authentication/Signup";
+export { User } from "./User/User";
