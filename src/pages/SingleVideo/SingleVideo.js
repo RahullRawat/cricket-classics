122fcabb923b5eef8b4360becfa535d8fbf5d27a
Rahul Rawat <rahulsinghrawat760@gmail.com>
2022-04-27 02:22:37 +0530

    feat:  single video page

---
+++ src/pages/SingleVideo/SingleVideo.js
@@ -0,0 +1,55 @@
+import React, { useEffect, useState } from "react";
+import { Sidebar } from "../../components";
+import { useParams } from "react-router-dom";
+import ReactPlayer from "react-player/lazy";
+import { getVideos } from "../../services/getVideos";
+import "./SingleVideo.css";
+
+export const SingleVideo = () => {
+	const [singlePageVideo, setSinglePageVideo] = useState([]);
+	const params = useParams();
+
+	useEffect(() => {
+		getVideos(setSinglePageVideo);
+	}, []);
+
+	const selectedVideo =
+		singlePageVideo.length !== 0 &&
+		singlePageVideo.find((video) => video._id === params.id);
+
+	return (
+		<div>
+			<Sidebar />
+			<div className="single-video-container ">
+				<div className="single-video">
+					<div className="video-player">
+						<ReactPlayer
+							className="react-player"
+							url={`https://www.youtube.com/watch?v=${params.id}`}
+							width="100%"
+							height="100%"
+							controls={true}
+						/>
+					</div>
+					<div className="single-video-info-container">
+						<div className="creator-info">
+							<h6>{selectedVideo.title}</h6>
+							{/* <span>90m views</span> */}
+						</div>
+						<div className="video-controls-container">
+							<button>
+								<i className="fa-solid fa-thumbs-up"></i>Like
+							</button>
+							<button>
+								<i className="fa-solid fa-clock"></i>Watch Later
+							</button>
+							<button>
+								<i className="fa-solid fa-list-check"></i>Playlists
+							</button>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
