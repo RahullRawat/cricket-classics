624e4226bff2b23d606c0dc55b7226067bb50464
Rahul Rawat <rahulsinghrawat760@gmail.com>
2022-04-27 02:21:22 +0530

    fix : changed link path

--- src/components/VideoCard/VideoCard.js
+++ src/components/VideoCard/VideoCard.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./VideoCard.css";
 
 export const VideoCard = ({
@@ -13,10 +14,12 @@ export const VideoCard = ({
 }) => {
 	return (
 		<div className="video-card-container">
-			<div className="thumbnail-container">
-				<img src={thumbnail} alt="video-thumbnail" />
-				<small className="timestamp">{time}</small>
-			</div>
+			<Link to={`/${_id}`}>
+				<div className="thumbnail-container">
+					<img src={thumbnail} alt="video-thumbnail" />
+					<small className="timestamp">{time}</small>
+				</div>
+			</Link>
 			<div className="video-info-container">
 				<img src={channelAvatar} className="avatar" alt="avatar" />
 				<div className="video-info">
