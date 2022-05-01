7015646a7e7eea63b1d6b85f480bc15642836318
Rahul Rawat <rahulsinghrawat760@gmail.com>
2022-05-02 01:09:03 +0530

    feat : delete like handler

--- src/components/HorizontalVideoCard/HorizontalVideoCard.js
+++ src/components/HorizontalVideoCard/HorizontalVideoCard.js
@@ -1,24 +1,32 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { deleteFromLike } from "../../services/deleteFromLikes";
+import { useAuth } from "../../context/AuthContext";
+import { useLike } from "../../context/LikeContext";
 import "./HorizontalVideoCard.css";
 
-export const HorizontalVideoCard = () => {
+export const HorizontalVideoCard = ({ video }) => {
+	const {
+		authState: { token },
+	} = useAuth();
+	const { likeDispatch } = useLike();
 	return (
 		<div className="horizontal-video-card">
-			<div className="thumbnail-container">
-				<img
-					src="https://i3.ytimg.com/vi/AFEZzf9_EHk/maxresdefault.jpg"
-					alt="video-thumbnail"
-				/>
-				<small className="timestamp">12.01</small>
-			</div>
+			<Link to={`/${video._id}`}>
+				<div className="thumbnail-container">
+					<img src={video.thumbnail} alt="video-thumbnail" />
+					<small className="timestamp">{video.time}</small>
+				</div>
+			</Link>
 			<div className="video-info">
-				<div className="video-title">
-					Stars crush Hurricanes as Maxwell makes history | BBL|11
-				</div>
+				<div className="video-title">{video.title}</div>
 				<div className="video-details">
-					<h6>ICC</h6>
+					<h6>{video.channelName}</h6>
 				</div>
-				<div className="delete-video-icon">
+				<div
+					className="delete-video-icon"
+					onClick={() => deleteFromLike(video._id, token, likeDispatch)}
+				>
 					<i class="fa-solid fa-trash-can"></i>
 				</div>
 			</div>
