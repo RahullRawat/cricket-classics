fb9df74ffaf791aa86ebefa11aa088ada7b51e25
Rahul Rawat <rahulsinghrawat760@gmail.com>
2022-05-08 13:52:14 +0530

    feat : exported single playlist page and playlist modal

--- src/components/index.js
+++ src/components/index.js
@@ -2,3 +2,5 @@ export { Navbar } from "./Navbar/Navbar";
 export { Sidebar } from "./Sidebar/Sidebar";
 export { VideoCard } from "./VideoCard/VideoCard";
 export { HorizontalVideoCard } from "./HorizontalVideoCard/HorizontalVideoCard";
+export { PlaylistModal } from "./PlaylistModal/PlaylistModal";
+export { SinglePlaylist } from "./SinglePlaylist/SinglePlaylist";
