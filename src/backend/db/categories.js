bc7372eb3210bb9b1f5056a5ee991414a87b6a46
Rahul Rawat <rahulsinghrawat760@gmail.com>
2022-05-10 23:20:27 +0530

    feat : added categories to backend

--- src/backend/db/categories.js
+++ src/backend/db/categories.js
@@ -6,22 +6,24 @@ import { v4 as uuid } from "uuid";
  * */
 
 export const categories = [
-  {
-    _id: uuid(),
-    categoryName: "Computer Programming",
-    description:
-      "Computer programming is the process of designing and building an executable computer program to accomplish a specific computing",
-  },
-  {
-    _id: uuid(),
-    categoryName: "Frontend Development",
-    description:
-      "Front-end web development, also known as client-side development is the practice of producing HTML, CSS and JavaScript for a website or Web Application",
-  },
-  {
-    _id: uuid(),
-    categoryName: "Backend Development",
-    description:
-      "Backend Development is also known as server-side development. It is everything that the users don't see and contains behind-the-scenes activities that occur when performing any action on a website.",
-  },
+	{
+		_id: uuid(),
+		categoryName: "BBL",
+	},
+	{
+		_id: uuid(),
+		categoryName: "CPL T20",
+	},
+	{
+		_id: uuid(),
+		categoryName: "The Hundred",
+	},
+	{
+		_id: uuid(),
+		categoryName: "ICC",
+	},
+	{
+		_id: uuid(),
+		categoryName: "T10 League",
+	},
 ];
