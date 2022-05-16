e70e14be6eff3af1337d104b256c1c575f9b8d1e
Rahul Rawat <rahulsinghrawat760@gmail.com>
2022-05-16 19:21:55 +0530

    feat: add to watch later from home

--- src/components/VideoCard/VideoCard.js
+++ src/components/VideoCard/VideoCard.js
@@ -1,34 +1,70 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { addToWatchLater } from "../../services/addToWatchLater";
+import { deleteFromWatchLater } from "../../services/deleteFromWatchLater";
+import { useWatchLater } from "../../context/WatchLaterContext";
+import { useAuth } from "../../context/AuthContext";
+import { toast } from "react-toastify";
 import "./VideoCard.css";
 
-export const VideoCard = ({
-	thumbnail,
-	channelAvatar,
-	channelName,
-	likes,
-	time,
-	title,
-	videoViews,
-	_id,
-}) => {
+export const VideoCard = ({ video }) => {
+	const [modal, setModal] = useState(false);
+	const navigate = useNavigate();
+
+	const {
+		authState: { token },
+	} = useAuth();
+
+	const {
+		watchLaterState: { watchLater },
+		watchLaterDispatch,
+	} = useWatchLater();
+
+	const isWatchLater = watchLater.some(
+		(alreadyPresentVideo) => alreadyPresentVideo._id === video._id
+	);
+	const watchLaterHandler = (id) => {
+		setModal((prev) => !prev);
+		if (!isWatchLater) {
+			if (token) {
+				addToWatchLater(video, token, watchLaterDispatch);
+				toast.success(`${video.title.slice(0, 20)}... added to watch later`);
+			} else {
+				navigate("/login");
+			}
+		} else {
+			deleteFromWatchLater(id, token, watchLaterDispatch);
+			toast.error(`${video.title.slice(0, 20)}... removed from watch later`);
+		}
+	};
 	return (
 		<div className="video-card-container">
-			<Link to={`/${_id}`}>
+			<Link to={`/${video._id}`}>
 				<div className="thumbnail-container">
-					<img src={thumbnail} alt="video-thumbnail" />
-					<small className="timestamp">{time}</small>
+					<img src={video.thumbnail} alt="video-thumbnail" />
+					<small className="timestamp">{video.time}</small>
 				</div>
 			</Link>
 			<div className="video-info-container">
-				<img src={channelAvatar} className="avatar" alt="avatar" />
+				<img src={video.channelAvatar} className="avatar" alt="avatar" />
 				<div className="video-info">
-					<div className="video-title">{title}</div>
+					<div className="video-title">{video.title}</div>
 					<div className="video-details">
-						<h6>{channelName}</h6>
-						<span>{videoViews}</span>
+						<h6>{video.channelName}</h6>
+						<span>{video.videoViews}</span>
 					</div>
 				</div>
+				<i
+					class="fa-solid fa-ellipsis-vertical"
+					onClick={() => setModal((prev) => !prev)}
+				></i>
+				{modal && (
+					<ul className="modal-container">
+						<li onClick={() => watchLaterHandler(video._id)}>
+							{isWatchLater ? "Remove from watch later" : "Add to watch later"}
+						</li>
+					</ul>
+				)}
 			</div>
 		</div>
 	);
